98272a33144a26302768553113f758ce0b5d9f0b
Anandkumar827 <112844743+Anandkumar827@users.noreply.github.com>
2022-09-05 14:05:12 +0530

    Add files via upload

---
+++ greatestnum.py
@@ -0,0 +1,11 @@
+print("printing greatest number among three")
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+c=int(input("enter the third number:"))
+if(a<c):
+    if(a<b)and (b>c):
+        print(b,"number is greatest")
+    else:
+        print(c,"number is greatest")
+else:
+    print(a,"number is greatest")
